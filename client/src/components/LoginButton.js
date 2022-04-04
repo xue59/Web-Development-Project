8ebe5b552be7661ff3f6b3c63551c4e20cab01b4
JunyanLing1 <ling.ju@northeastern.edu>
2022-04-04 16:41:28 -0700

    allow edit and delete

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -3,7 +3,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export default function LoginButton() {
   const { loginWithRedirect } = useAuth0();
-  console.log("???");
 
   return (
     <button
