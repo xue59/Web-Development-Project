d9f3dd3a685a3498b6b54eabd304b5f2256e46e4
evanscho97 <evanscho97@gmail.com>
2022-04-12 19:10:34 -0700

    Search API finished

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -23,7 +23,7 @@ export default function Search() {
  
  // This function will handle the submission.
  async function onSubmit(e) {
-
+  navigate(`/search/${form.itemName}`)
     // TODO: add search logic below
 
   // const params = useParams();
@@ -37,7 +37,7 @@ export default function Search() {
   //     console.log(inventory);
   //   }
   //   findInventory();
-  // }, [params.id]);
+  // }, [params.itemName]);
  }
  
  // This following section will display the form that takes the input from the user.
@@ -55,71 +55,6 @@ export default function Search() {
            onChange={(e) => updateForm({ itemName: e.target.value })}
          />
        </div>
-{/* 
-       <div className="form-group">
-         <label htmlFor="subTeams">subTeams:</label>
-         <input
-           type="text"
-           className="form-control"
-           id="subTeams"
-           value={form.subTeams}
-           onChange={(e) => updateForm({ subTeams: e.target.value })}
-         />
-       </div> */}
-      
-       {/* <div className="form-group">
-         <p>Region: </p>
-         <div className="form-check form-check-inline">
-           <input
-             className="form-check-input"
-             type="radio"
-             name="regionEast"
-             id="region-East"
-             value="East"
-             checked={form.region === "East"}
-             onChange={(e) => updateForm({ region: e.target.value })}
-           />
-           <label htmlFor="regionEast" className="form-check-label">East</label>
-         </div>
-
-         <div className="form-check form-check-inline">
-           <input
-             className="form-check-input"
-             type="radio"
-             name="regionMiddle"
-             id="region-Middle"
-             value="Middle"
-             checked={form.region === "Middle"}
-             onChange={(e) => updateForm({ region: e.target.value })}
-           />
-           <label htmlFor="regionMiddle" className="form-check-label">Middle</label>
-         </div>
-
-         <div className="form-check form-check-inline"> 
-           <input
-             className="form-check-input"
-             type="radio"
-             name="regionWest"
-             id="region-West"
-             value="West"
-             checked={form.region === "West"}
-             onChange={(e) => updateForm({ region: e.target.value })}
-           />
-           <label htmlFor="regionWest" className="form-check-label">West</label>
-         </div>
-
-         <div className="form-group">
-         <label htmlFor="warehouseLocation">warehouseLocation: </label>
-         <input
-           type="text"
-           className="form-control"
-           id="warehouseLocation"
-           value={form.warehouseLocation}
-           onChange={(e) => updateForm({ warehouseLocation: e.target.value })}
-         />
-        </div> */}
-       {/* </div> */}
-
        <div className="form-group">
          <input
            type="submit"
